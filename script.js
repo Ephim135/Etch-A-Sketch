187d6c38bf73ed6e5638ea30f06f24cdf1e3d031
Fabian Berndt <fabian009.berndt@gmail.com>
2022-12-20 20:00:07 +0100

    add functions to buttons better styling

--- script.js
+++ script.js
@@ -1,15 +1,97 @@
-function createDivs (){
-for (let row = 0; row < 16 ; row++){
-    for (let column = 0; column < 16; column++){
+
+
+const sizeSlide = document.getElementById("sizeSlide");
+const container = document.getElementById("container");
+const grid = document.getElementsByClassName("grid");
+const colorPicker = document.getElementById("color");
+let color = "#ff0000";
+const clearButton = document.getElementById("clear");
+const rainbowButton = document.getElementById("rainbowMode");
+const borderButton = document.getElementById("border");
+
+
+// color Picker
+colorPicker.addEventListener("input", function(event){
+    color = event.target.value;
+    setColor();
+    });
+
+function setColor() { 
+for (let i = 0; i < grid.length; i++){
+    grid[i].addEventListener("mouseover", function(e){
+        e.target.style.backgroundColor = color; 
+    })}};
+
+// clear button
+clearButton.addEventListener("click", function(){
+    for (let i = 0; i < grid.length; i++){
+        grid[i].style.backgroundColor = "white";
+    }
+});
+
+//Rainbow Button
+rainbowButton.addEventListener("click", function(){
+    if (rainbowButton.style.backgroundColor == "green"){
+        rainbowButton.style.backgroundColor = "";
+        setColor();
+    }
+    else {
+        rainbowButton.style.backgroundColor = "green";
+        setColorRainbow();
+    }});
+
+function setColorRainbow() { 
+    for (let i = 0; i < grid.length; i++){
+        grid[i].addEventListener("mouseover", function(e){
+            e.target.style.backgroundColor = getRandomColor(); 
+        })}};
+
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let randomColor = "#";
+    for (let i = 0; i < 6; i++) {
+        randomColor += letters[Math.floor(Math.random() * 16)];
+    }
+    return randomColor
+}
+// Border button
+borderButton.addEventListener("click", function(){
+    if (borderButton.style.backgroundColor == "green"){
+        borderButton.style.backgroundColor = "";
+        for (let i = 0; i < grid.length; i++){
+            grid[i].style.border = "0px solid white";
+        }
+    } 
+    else {
+        borderButton.style.backgroundColor = "green";
+        
+        for (let i = 0; i < grid.length; i++){
+            grid[i].style.border = "1px solid white";
+        }
+
+}});
+
+sizeSlide.addEventListener("change", function(){
+    container.innerHTML = ""; //delete pervious Grid
+    container.style.gridTemplateColumns = `repeat(${sizeSlide.value}, auto)`; // create rows/columns
+    container.style.gridTemplateRows = `repeat(${sizeSlide.value}, auto)`;
+    createDivs (sizeSlide.value, sizeSlide.value);  // create grid
+    document.getElementById("sizeSlideValue").textContent = sizeSlide.value;
+    setColor();
+    borderButton.click(); // reset borders for new amount of divs
+    borderButton.click();
+});
+
+
+function createDivs (row, column){
+for (let r = 0; r < row ; r++){
+    for (let c = 0; c < column; c++){
         const newDiv = document.createElement("div");
-        const newContent = document.createTextNode("1");
+        newDiv.setAttribute("class", "grid"); 
+        const newContent = document.createTextNode("");
         newDiv.appendChild(newContent);
         document.getElementById("container").appendChild(newDiv);
     }}};
 
-function createDiv (){
-
-    const newDiv = document.createElement("div");
-
-    document.getElementById("container").appendChild(newDiv);
-}
+createDivs(16, 16);
+setColor();
